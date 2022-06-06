9d95c7f374229d85fd55c0d4385b2eb102397765
mfc003 <mfatihcatal1903@gmail.com>
2022-06-06 21:02:37 +0300

    Update main.dart
    Son hali yuklendi,
    sayac/pomodoro eklendi,
    ui/ix duzenleme yapildi

--- To_do_list_v1/lib/main.dart
+++ To_do_list_v1/lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:timer_count_down/timer_count_down.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -9,19 +11,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "What To Do Uygulaması",
-      home: Iskele(),
+      title: "What To Do B91 Team",
+      theme: ThemeData(
+
+        primarySwatch: Colors.green,
+      ),
+      home: Taslak(),
     );
   }
 }
 
-class Iskele extends StatelessWidget {
+class Taslak extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(
-          child: Text('What To Do')
+            child: Text('Görev Listesi',
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.w400,
+                ))
         ),
       ),
       body: AnaEkran(),
@@ -57,17 +67,16 @@ class _AnaEkranState extends State<AnaEkran> {
     Navigator.of(context).push(
         MaterialPageRoute(
           builder: (context) {
-            return Scaffold(
-              appBar:  AppBar(),
-              body:
-            FortuneWheel(
-              selected: controller.stream,
-              items: [
-              for (var i in yapilacaklarListesi) FortuneItem(child: Text(i)),
-              ],
-            )
-            );
-    },));
+            return Cark_Ekrani(yapilacaklarListesi: yapilacaklarListesi);
+          },));
+  }
+
+  sayacaGit(){
+    Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return Sayac_Sayfasi();
+          },));
   }
 
   @override
@@ -80,7 +89,12 @@ class _AnaEkranState extends State<AnaEkran> {
               itemCount: yapilacaklarListesi.length,
               itemBuilder: (context, siraNumarasi) => ListTile(
                 subtitle: Text(""),
-                title: Text(yapilacaklarListesi[siraNumarasi]),
+                title: Text(yapilacaklarListesi[siraNumarasi],
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.italic,
+                    )),
               ),
             ),
           ),
@@ -89,16 +103,108 @@ class _AnaEkranState extends State<AnaEkran> {
           ),
           ElevatedButton(
             onPressed: elemanEkle,
-            child: Text("Ekle"),
+            child: Text("Görev ekle",
+    style: TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+    )),
           ),
           ElevatedButton(
             onPressed: elemanCikar,
-            child: Text("Çıkar"),
+            child: Text("Görev çıkar",
+    style: TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+    )),
           ),
           ElevatedButton(
-              onPressed: carkaGit,
-              child: Text("Çarka Git"),
-          )
+            onPressed: carkaGit,
+            child: Text("Çarka git",
+            style: TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+    )),
+          ),
+          ElevatedButton(
+            onPressed: sayacaGit,
+            child: Text("Sayacı başlat",
+    style: TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+    )),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Sayac_Sayfasi extends StatelessWidget {
+  const Sayac_Sayfasi({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sayaç / Pomodoro"),
+      ),
+      body: Center(
+
+        child: (
+        Countdown(
+
+          seconds: 1500,
+          build: (
+              BuildContext context, time) =>
+              Text(
+
+                  time.toString(),
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.w200,
+                )
+
+              ),
+
+          interval:
+          Duration(milliseconds: 100),
+          onFinished: () {
+            print('25 dakika sona erdi, tebrikler!');
+          },
+        )
+    ),
+      ),
+    );
+  }
+}
+
+class Cark_Ekrani extends StatelessWidget {
+  const Cark_Ekrani({
+    Key? key,
+    required this.yapilacaklarListesi,
+  }) : super(key: key);
+
+  final List yapilacaklarListesi;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Görev Çarkı"),
+      ),
+      body:
+      FortuneWheel(
+        selected: controller.stream,
+        items: [
+          for (var i in yapilacaklarListesi) FortuneItem(child:
+          Text(i,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              )
+          )),
         ],
       ),
     );
